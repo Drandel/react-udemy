539251a130d6a914db80614ee53ea038c66c0c1c
Dean Randel <dee.randel@gmail.com>
2022-10-13 13:05:53 -0600

    Updates

---
+++ precache-manifest.78b5eb512a052cdb714e77424d348789.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58b0d81d4b6d6cc7b705",
+    "url": "/react-udemy/static/css/main.b3ae94b1.chunk.css"
+  },
+  {
+    "revision": "58b0d81d4b6d6cc7b705",
+    "url": "/react-udemy/static/js/main.58b0d81d.chunk.js"
+  },
+  {
+    "revision": "a1d0caba43df50663965",
+    "url": "/react-udemy/static/js/1.a1d0caba.chunk.js"
+  },
+  {
+    "revision": "79bc4628e3c051396cf4",
+    "url": "/react-udemy/static/js/runtime~main.79bc4628.js"
+  },
+  {
+    "revision": "71c728cfb047928dad378a6d5a20c25e",
+    "url": "/react-udemy/index.html"
+  }
+];
